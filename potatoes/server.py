0a18c834e27b273397ba1d0a0ecfc373f806d47e
xtian <christian@nyu.edu>
2023-05-11 14:41:41 -0400

    Add potatoes directory

---
+++ potatoes/server.py
@@ -0,0 +1,10 @@
+import http.server
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        super().end_headers()
+
+if __name__ == '__main__':
+    http.server.test(HandlerClass=MyHTTPRequestHandler)
